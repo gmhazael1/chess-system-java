a15aa1a4a1b999221e7320eba5a1afd0553fca3e
Hazael <gmhazael1@gmail.com>
2023-09-12 19:43:02 -0300

    Implementing possible moves of Rook

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -2,45 +2,45 @@ package boardgame;
 
 public class Board {
     private int rows;
-    private int colums;
+    private int columns;
     private Piece[][] pieces;
 
-    public Board(int rows, int colums) {
-        if (rows < 1 || colums < 1){
-            throw new BoardException("Error creating board: there must be at least 1 row and 1 colum.");
+    public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1){
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column.");
         }
         this.rows = rows;
-        this.colums = colums;
-        pieces = new Piece[rows][colums];
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
     }
 
     public int getRows() {
         return rows;
     }
 
-    public int getColums() {
-        return colums;
+    public int getColumns() {
+        return columns;
     }
 
-    public Piece piece(int row, int colum){
-        if (!positionExists(row, colum)){
+    public Piece piece(int row, int column){
+        if (!positionExists(row, column)){
             throw new BoardException("Position not on the Board");
         }
-        return pieces[row][colum];
+        return pieces[row][column];
     }
 
     public Piece piece(Position position){
         if (!positionExists(position)){
             throw new BoardException("Position not on the Board");
         }
-        return pieces[position.getRow()][position.getColum()];
+        return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
         if (thereIsAPiece(position)){
             throw new BoardException("There is already a piece on position " + position);
         }
-        pieces[position.getRow()][position.getColum()] = piece;
+        pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
@@ -53,16 +53,16 @@ public class Board {
         }
         Piece aux = piece(position);
         aux.position = null;
-        pieces[position.getRow()][position.getColum()] = null;
+        pieces[position.getRow()][position.getColumn()] = null;
         return aux;
     }
 
-    private Boolean positionExists(int row, int colum){
-        return row >= 0 && row < rows && colum >= 0 && colum < colums;
+    private Boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 
     public Boolean positionExists(Position position){
-        return positionExists(position.getRow(), position.getColum());
+        return positionExists(position.getRow(), position.getColumn());
     }
 
     public Boolean thereIsAPiece(Position position){
