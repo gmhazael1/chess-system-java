c1bca5315a95cdd462806d7007c3299b3e24ccd0
Hazael <gmhazael1@gmail.com>
2023-09-16 07:45:13 -0300

    Checkmate logic

--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -2,6 +2,7 @@ package aplication;
 
 import chess.*;
 
+import javax.imageio.plugins.tiff.TIFFDirectory;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -52,10 +53,17 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 
-        if (chessMatch.getCheck()){
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
+        }
+        else {
+            System.out.println("CHECKMATE!");
+            System.out.println("WINNER: " + chessMatch.getCurrentPlayer());
         }
     }
 
